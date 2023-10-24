a3b4865276aa4b00f8bde812b76450aad3c1cb15
12amirAhm <a4522ahmed@gmail.com>
2023-10-24 02:32:54 -0700

    the mandatory tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Creates an empty class called Square """
+
+
+class Square:
+    """ Empty class with size private attribute """
+    def __init__(self, size):
+
+        self.__size = size
