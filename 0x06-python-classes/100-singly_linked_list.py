1ca3362b3722787d3c75884b4730a6d31aea8410
12amirAhm <a4522ahmed@gmail.com>
2023-10-24 04:41:23 -0700

    the updated one

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,32 +1,61 @@
 #!/usr/bin/python3
+""" Creates an empty class called Node
+"""
+
 class Node:
+     """ Empty class with size private attribute
+    """
     def __init__(self, data, next_node=None):
+         """
+                Instantiation with size
+        Args:
+            size: size of the Node
+            position: postion of the square
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """
+        Returns the data
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        Returns the data
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """
+        Returns the data
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        Returns the next_node of the Node
+        """
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """
+         this is the  singlylistedlis
+        """
     def __str__(self):
+        """
+        Returns the self
+        """
         rtn = ""
         ptr = self.__head
 
@@ -39,9 +68,15 @@ class SinglyLinkedList:
         return rtn
 
     def __init__(self):
+        """
+        Returns the head
+        """
         self.__head = None
 
     def sorted_insert(self, value):
+        """
+        Returns the sorted_insert
+        """
         ptr = self.__head
 
         while ptr is not None:
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import math
+""" Creates class called MagicClass
+"""
+
+
 
 
 class MagicClass:
@@ -7,6 +11,9 @@ class MagicClass:
     """Class that stores the properties
     of a circumference"""
     def __init__(self, radius=0):
+       """
+        Returns the self
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
@@ -14,8 +21,14 @@ class MagicClass:
 
     """ Method that calculates the area of the circumference """
     def area(self):
+        """
+        Returns the area of the magicClass
+        """
         return ((self.__radius ** 2) * math.pi)
 
     """ Method that calculates the perimeter of a circumference """
     def circumference(self):
+        """
+        Returns the cicumference
+        """
         return (2 * math.pi * self.__radius)
